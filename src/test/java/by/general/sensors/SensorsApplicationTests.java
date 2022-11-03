bfc21a93a015dc9b92144032126d2218ae324ab7
General11111 <43624961+General11111@users.noreply.github.com>
2022-11-03 13:58:56 +0300

    Start app

---
+++ src/test/java/by/general/sensors/SensorsApplicationTests.java
@@ -0,0 +1,13 @@
+package by.general.sensors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
